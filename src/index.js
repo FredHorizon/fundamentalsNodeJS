a7f70f6465429c2423a12477bae8fd59fa891019
FredHorizon <fredoliveira.sys@gmail.com>
2020-12-11 17:09:26 -0400

    Config Nodemon

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const app = express();
 
 app.get('/projects', (req, res) => {
   // return res.send('Teste de rota ok');
-  return res.json({ message: 'Teste rota com json.' })
+  return res.json({ message: 'Essa é a page \'projects\'' })
 })
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('Server started => localhost:3333')
+});
