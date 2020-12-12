256a11d25ced982242807655aac13a7a4eea9a8b
FredHorizon <fredoliveira.sys@gmail.com>
2020-12-12 10:33:58 -0400

    Creates HTTP routes

--- src/index.js
+++ src/index.js
@@ -4,9 +4,29 @@ const app = express();
 
 app.get('/projects', (req, res) => {
   // return res.send('Teste de rota ok');
-  return res.json({ message: 'Essa é a page \'projects\'' })
-})
+  return res.json({ message: 'Essa é a page \'projects\'' });
+});
+
+app.post('/projects', (req, res) => {
+  return res.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
+});
+
+app.put('/projects/:id', (req, res) => {
+  return res.json([
+    'Projeto 3',
+    'Projeto 2',
+  ]);
+});
+
+app.delete('/projects/:id', (req, res) => {
+  return res.json([
+    'Projeto 2',
+  ]);
+});
 
 app.listen(3333, () => {
-  console.log('Server started => localhost:3333')
+  console.log('Server started => localhost:3333');
 });
